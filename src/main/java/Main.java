22959c60ff5f6d344bd7e916722de3dd0ee345fd
ramimhossain12 <ramimhossain6242@gmail.com>
2020-12-02 22:31:56 +0600

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import io.reactivex.Observable;
+import io.reactivex.functions.Consumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+     Thread thread = new Thread(()-> {
+         runIt(); 
+     });
+     };
+
+    public  static  void  runIt(){
+
+    }
+}
+
